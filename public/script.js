3b764ca0b4f1d13f1e2c9714e42f93b31302c342
lyndon dsouza <lyndondsouza1@gmail.com>
2021-08-13 09:51:05 +0530

    respon 50%

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
 const setTheme = (theme) => (document.documentElement.className = theme);
+
+const hamburger = document.querySelector(".hamburger");
+const filters = document.querySelector(".filters");
+
+function myFunction() {
+	filters.classList.toggle("active");
+}
